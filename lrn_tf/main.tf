9b091480ee9eacbd51c4a47d2a67c11661ede11d
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-12 15:13:23 +0530

    Create main.tf to understand for_each

---
+++ lrn_tf/main.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "stu_marks" {
+  for_each = var.stu_marks
+  provisioner "local_exec" {
+    command = " echo student name --> ${each.key} and ${each.value} "
+  }
+}
+
+variable "stu_marks" {
+  default = {
+    name = "ragh"
+    s1 = 20
+    s2 = 24
+    s3 = 23
+  }
+}
