dcb1d840d21736b36cf159ee5e5a57e380974ca7
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-12 15:51:12 +0530

    Create servers.tf with t2.micro

---
+++ 02-05/servers.tf
@@ -0,0 +1,75 @@
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t2.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t2.micro"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t2.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t2.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t2.micro"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t2.micro"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t2.micro"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t2.micro"
+    }
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+
+resource "aws_route53_record" "records" {
+  for_each               = var.components
+  zone_id = "Z03986262CQPCHNJNZM9L"
+  name    = "${each.value["name"]}-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.value["name"]].private_ip]
+}
